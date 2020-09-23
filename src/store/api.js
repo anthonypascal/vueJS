aa8aa629ea4ac8fb96248b5a957deeda04df4678
Axel Pelerin <arcanynka@gmail.com>
2020-09-23 14:36:29 +0200

    Add genre checkboxes functionality, including the store API

--- src/store/api.js
+++ src/store/api.js
@@ -1,17 +1,28 @@
 import axios from 'axios'
-
+import {movieDB_api_key} from '@/config';
 
 export default ({
+  namespaced: true,
 
   state: {
-
+    api_url: 'https://api.themoviedb.org/3/',
+    img_url: 'http://image.tmdb.org/t/p/w500/',
+    movieDB_api_key: movieDB_api_key,
+    list_genre: [],
   },
 
   mutations: {
+    setListGenre(state, list) {
+      state.list_genre = list
+    }
 
   },
 
   actions: {
+    getGenre({commit, state}) {
+      axios.get(state.api_url + 'genre/movie/list?api_key=' + movieDB_api_key)
+          .then(response => { commit('setListGenre', response.data.genres) })
+    }
   },
 
   getters: {
--- src/views/SearchMovie.vue
+++ src/views/SearchMovie.vue
@@ -1,11 +1,27 @@
 <template>
 <div class="d-flex flex-row justify-content-center">
-	<div class="form-check">
-	    <input type="checkbox" class="form-check-input" id="materialUnchecked">
-	    <label class="form-check-label" for="materialUnchecked">This will be a category</label>
+	<div v-for="genre in list_genre" :key="genre.id" class="form-check">
+	    <input :value="genre.id" type="checkbox" class="form-check-input" id="materialUnchecked">
+	    <label class="form-check-label" for="materialUnchecked">{{ genre.name }}</label>
 	</div>
 	<div class="md-form active-cyan active-cyan-2 mb-3 ml-4">
 	  	<input class="form-control" type="text" placeholder="Search" aria-label="Search">
 	</div>
 </div>
 </template>
+
+<script>
+	import { mapState } from 'vuex'
+	export default {
+
+		computed: {
+			...mapState({
+				list_genre: state => state.api.list_genre,
+			}),
+		},
+
+		mounted() {
+			this.$store.dispatch("api/getGenre");
+		}
+	}
+</script>
