ce25b160c8f103ce7f40e569ee28222fbca45ac0
Axel Pelerin <arcanynka@gmail.com>
2020-09-23 16:34:56 +0200

    Add search by genre(s)

--- src/store/api.js
+++ src/store/api.js
@@ -9,6 +9,7 @@ export default ({
     img_url: 'http://image.tmdb.org/t/p/w500/',
     movieDB_api_key: movieDB_api_key,
     list_genre: [],
+    result_search_by_genre: []
   },
 
   mutations: {
@@ -21,7 +22,31 @@ export default ({
   actions: {
     getGenre({commit, state}) {
       axios.get(state.api_url + 'genre/movie/list?api_key=' + movieDB_api_key)
-          .then(response => { commit('setListGenre', response.data.genres) })
+          .then(response =>
+          {
+            var temp_list = response.data.genres;
+            for (var i = 0; i < temp_list.length; i++) {
+              temp_list[i].check = false;
+            }
+
+            commit('setListGenre', temp_list)
+          }
+          )
+    },
+
+    getMovieByGenre({commit, state}) {
+      var list_id = []
+      for(var i = 0; i < state.list_genre.length; i++) {
+        if (state.list_genre[i].check) {
+          list_id.push(state.list_genre[i].id)
+        }
+      }
+      console.log(list_id)
+      axios.get(state.api_url + 'discover/movie?api_key=' + movieDB_api_key + '&with_genres=' + list_id.join(','))
+          .then(response => {
+            console.log(response.data)
+            state.result_search_by_genre = response.data })
+
     }
   },
 
--- src/views/SearchMovie.vue
+++ src/views/SearchMovie.vue
@@ -1,9 +1,10 @@
 <template>
-<div class="d-flex flex-row justify-content-center">
-	<div v-for="genre in list_genre" :key="genre.id" class="form-check">
-	    <input :value="genre.id" type="checkbox" class="form-check-input" id="materialUnchecked">
+<div class="justify-content-center">
+	<div v-for="(genre, index) in list_genre" :key="index" class="form-check">
+	    <input :value="index" type="checkbox" v-model="list_genre[index].check" class="form-check-input" id="materialUnchecked">
 	    <label class="form-check-label" for="materialUnchecked">{{ genre.name }}</label>
 	</div>
+	<button @click="searchByGenre">Apply filter</button>
 	<div class="md-form active-cyan active-cyan-2 mb-3 ml-4">
 	  	<input class="form-control" type="text" placeholder="Search" aria-label="Search">
 	</div>
@@ -17,11 +18,18 @@
 		computed: {
 			...mapState({
 				list_genre: state => state.api.list_genre,
+				result_search_by_genre: state => state.api.result_search_by_genre
 			}),
 		},
 
 		mounted() {
 			this.$store.dispatch("api/getGenre");
+		},
+
+		methods: {
+			searchByGenre() {
+				this.$store.dispatch("api/getMovieByGenre")
+			}
 		}
 	}
 </script>
