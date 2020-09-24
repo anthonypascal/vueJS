1d255f4e0a5cd7b257b2032a9e1b394ed66a03bf
Axel Pelerin <arcanynka@gmail.com>
2020-09-24 16:52:47 +0200

    Add sorting and missing category

--- src/components/CardMovie.vue
+++ src/components/CardMovie.vue
@@ -3,7 +3,11 @@
         <div class="card" style="width: 18rem;">
         <img class="card-img-top" :src="img_url + movie.poster_path" alt="Card image cap">
         <div class="card-body">
-            <p class="card-text"> {{ this.movie.title }} </p>
+            <p class="card-text"> {{ movie.title }} </p>
+            <div>
+                <p>Missing category of your research: </p>
+                <div v-for="missing_id in movie.notInGenreIds">{{ list_genre[missing_id].name }}</div>
+            </div>
         </div>
     </div>
     </router-link>
@@ -20,9 +24,11 @@
 
         computed: {
             ...mapState({
+                list_genre: state => state.api.list_genre,
                 img_url: state => state.api.img_url,
             }),
         },
+
     }
 </script>
 
